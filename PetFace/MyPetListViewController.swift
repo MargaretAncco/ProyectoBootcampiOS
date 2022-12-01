7a6ee241167e596b48a4353fac3c664b6609e571
margaret ancco <manccoca@emeal.nttdata.com>
2022-12-01 11:27:41 -0500

    modified navigation

--- PetFace/MyPetListViewController.swift
+++ PetFace/MyPetListViewController.swift
@@ -1,26 +1,21 @@
 //
-//  PetDetailViewController.swift
+//  MyPetListViewController.swift
 //  PetFace
 //
-//  Created by Margaret Alexia Ancco Calloapaza on 30/11/22.
+//  Created by Margaret Alexia Ancco Calloapaza on 1/12/22.
 //
 
 import UIKit
 
-class PetDetailViewController: UIViewController {
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var petImage: UIImageView!
-    @IBOutlet weak var isFavoriteImage: UIImageView!
-    @IBOutlet weak var likesCount: UILabel!
-    var selectedPet: Pet!
+class MyPetListViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUp(with: selectedPet)
+
         // Do any additional setup after loading the view.
     }
-    func setUp(with selectedPet: Pet){
-        //nameLabel.text = selectedPet.name
-    }
+    
+
     /*
     // MARK: - Navigation
 
